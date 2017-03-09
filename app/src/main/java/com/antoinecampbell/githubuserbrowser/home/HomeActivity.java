6e551682b7c28cad269e6dbd7a30333f7feb948b
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-03-08 20:14:35 -0500

    Documented HomeActivity Class
    Comments were added to allow for maintenance and step-by-step
    understanding of the the processes and functionality of the
    code; to make maintenance of the code for later android
    revisions simpler.

--- app/src/main/java/com/antoinecampbell/githubuserbrowser/home/HomeActivity.java
+++ app/src/main/java/com/antoinecampbell/githubuserbrowser/home/HomeActivity.java
@@ -1,22 +1,32 @@
 package com.antoinecampbell.githubuserbrowser.home;
 
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;                           // Import Android Operating System 
+import android.support.v7.app.ActionBarActivity;    // Import Android Application Activity 
+import com.antoinecampbell.githubuserbrowser.R;     // Import GitADate recourses 
 
-import com.antoinecampbell.githubuserbrowser.R;
-
+/*
+ * The Home View | The Default View
+ * The initial action that occurs when the application is opened
+ * Is an extension of the Android Activity type, allowing for system and user interaction
+ */
 public class HomeActivity extends ActionBarActivity {
 
+    /*
+     * App Launch - Activity
+     * Sets the default layout of the application to the home layout resource.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        super.onCreate(savedInstanceState);         // Run default onCreate actions
+        setContentView(R.layout.activity_home);     // Change the mobile view to the home layout
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.activity_home_fragment_container,
-                            HomeFragment.newInstance(), HomeFragment.class.getSimpleName())
-                    .commit();
+        // If a new instantiation is occurring then set up the Home Activity Fragments for the instance
+        if (savedInstanceState == null) {                                                       //if new instance of application
+
+            getSupportFragmentManager().beginTransaction()                                      // Begin an interaction with the applications Fragment Manager
+                    .replace(R.id.activity_home_fragment_container,                                    
+                            HomeFragment.newInstance(), HomeFragment.class.getSimpleName())     // Replace the default Fragment Manager with the HomeFragment Class    
+                    .commit();                                                                  // Finalize and initiate change/transaction 
         }
     }
 
